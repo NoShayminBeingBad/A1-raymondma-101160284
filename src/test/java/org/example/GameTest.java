449f7568e4b552e09737defe977e5b90f54dfe36
NoShayminTrying <37005204+NoShayminBeingBad@users.noreply.github.com>
2024-10-03 01:22:17 -0400

    R-TEST-RESP-3

--- src/test/java/org/example/GameTest.java
+++ src/test/java/org/example/GameTest.java
@@ -2,11 +2,15 @@ package org.example;
 
 import org.example.Card.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -105,6 +109,63 @@ class GameTest {
         }
 
         Assertions.assertTrue(game.getAdventureDeck().size() < adventureDeckStart);
+
+    }
+
+    @Test
+    @DisplayName("Test Win condition: 1 winner")
+    public void RESP_3_TEST_1(){
+
+        Scanner input = new Scanner("");
+        StringWriter output = new StringWriter();
+
+        Game game = new Game(4, input, new PrintWriter(output));
+
+        game.getPlayer(0).giveShield(3);
+        game.getPlayer(1).giveShield(1);
+        game.getPlayer(2).giveShield(6);
+        game.getPlayer(3).giveShield(7);
+
+        Assertions.assertTrue(game.outputWinner());
+        Assertions.assertTrue(output.toString().contains("Player 4 has reached 7 shields!"));
+
+    }
+
+    @Test
+    @DisplayName("Test Win condition: Multiple winner")
+    public void RESP_3_TEST_2(){
+
+        Scanner input = new Scanner("");
+        StringWriter output = new StringWriter();
+
+        Game game = new Game(4, input, new PrintWriter(output));
+
+        game.getPlayer(0).giveShield(0);
+        game.getPlayer(1).giveShield(8);
+        game.getPlayer(2).giveShield(7);
+        game.getPlayer(3).giveShield(6);
+
+        Assertions.assertTrue(game.outputWinner());
+        Assertions.assertTrue(output.toString().contains("Player 2 has reached 7 shields!"));
+        Assertions.assertTrue(output.toString().contains("Player 3 has reached 7 shields!"));
+    }
+
+    @Test
+    @DisplayName("Test Win condition: No winner")
+    public void RESP_3_TEST_3(){
+
+        Scanner input = new Scanner("");
+        StringWriter output = new StringWriter();
+
+        Game game = new Game(4, input, new PrintWriter(output));
+
+        game.getPlayer(0).giveShield(3);
+        game.getPlayer(1).giveShield(1);
+        game.getPlayer(2).giveShield(6);
+        game.getPlayer(3).giveShield(1);
+
+        Assertions.assertFalse(game.outputWinner());
+        Assertions.assertEquals("", output.toString());
 
     }
 }
