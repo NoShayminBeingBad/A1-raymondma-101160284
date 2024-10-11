eb3ea314692a5332492bfeae334551349ea1674c
NoShayminTrying <37005204+NoShayminBeingBad@users.noreply.github.com>
2024-10-11 19:42:37 -0400

    R-CODE-RESP-8

--- src/main/java/org/example/Card/QuestCard.java
+++ src/main/java/org/example/Card/QuestCard.java
@@ -2,6 +2,9 @@ package org.example.Card;
 
 import org.example.Game;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class QuestCard extends EventCard{
 
     private int stages;
@@ -21,6 +24,40 @@ public class QuestCard extends EventCard{
 
     @Override
     public void Event(Game game) {
+        Scanner input = game.getInput();
+        PrintWriter output = game.getOutput();
 
+        output.println(String.format("Quest Card has been drawn!\nThis Quest has %d stages", stages));
+
+
+        int sponsorCheck = game.getTurnCount();
+        int sponsor = -1;
+
+        for (int i = 0; i < game.playerAmount(); i++){
+            game.getPlayer(sponsorCheck).printHand(output);
+            output.print(String.format("Player %d, sponsor this quest? (y/n) ", sponsorCheck + 1));
+            String decision = input.nextLine();
+            output.println("");
+            if (decision.charAt(0) == 'y'){
+                sponsor = sponsorCheck;
+                break;
+            }else {
+                sponsorCheck = nextPlayer(sponsorCheck);
+                continue;
+            }
+        }
+
+        if (sponsor == -1){
+            output.println("No sponsor, quest has been canceled");
+            return;
+        }
+
+    }
+
+    public int nextPlayer(int i){
+        if (i+1 > 3){
+            return 0;
+        }
+        return i+1;
     }
 }
