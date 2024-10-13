17e75a411b9b8de6c6805334be47e0280c991d88
NoShayminTrying <37005204+NoShayminBeingBad@users.noreply.github.com>
2024-10-13 16:14:53 -0400

    R-TEST-RESP-13

--- src/test/java/org/example/Card/EventCardTest.java
+++ src/test/java/org/example/Card/EventCardTest.java
@@ -33,8 +33,27 @@ class EventCardTest {
     }
 
     @Test
-    @DisplayName("Player draws Queen's Favor card")
+    @DisplayName("Player draws Plague card and has less than 2 shields")
     public void RESP_6_TEST_2(){
+        Scanner input = new Scanner("");
+        StringWriter output = new StringWriter();
+
+        Game game = new Game(4, input, new PrintWriter(output));
+
+        game.setUpGame();
+
+        game.getPlayer(game.getTurnCount()).giveShield(1);
+
+        game.getEventDeck().getCards().set(0, new PlagueCard());
+        game.drawEventCard();
+        game.playEventCard();
+
+        Assertions.assertEquals(0, game.getPlayer(game.getTurnCount()).getShieldNum());
+    }
+
+    @Test
+    @DisplayName("Player draws Queen's Favor card")
+    public void RESP_13_TEST_1(){
         Scanner input = new Scanner("");
         StringWriter output = new StringWriter();
 
@@ -55,36 +74,8 @@ class EventCardTest {
     }
 
     @Test
-    @DisplayName("Player draws Prosperity card")
-    public void RESP_6_TEST_3(){
-        Scanner input = new Scanner("\n\n\n\n");
-        StringWriter output = new StringWriter();
-
-        Game game = new Game(4, input, new PrintWriter(output));
-        game.setUpGame();
-
-        game.getPlayer(0).discardCards(3);
-        game.getPlayer(1).discardCards(2);
-        game.getPlayer(2).discardCards(4);
-        game.getPlayer(3).discardCards(12);
-
-        int[] cardsBefore = {game.getPlayer(0).getHand().size(),
-                game.getPlayer(1).getHand().size(),
-                game.getPlayer(2).getHand().size(),
-                game.getPlayer(3).getHand().size()};
-
-        game.getEventDeck().getCards().set(0, new ProsperityCard());
-        game.drawEventCard();
-        game.playEventCard();
-
-        for (int i = 0; i < cardsBefore.length; i++) {
-            Assertions.assertEquals(cardsBefore[i] + 2, game.getPlayer(i).getHand().size());
-        }
-    }
-
-    @Test
     @DisplayName("Player draws Queen's Favor card and needs to discard 1 cards")
-    public void RESP_6_TEST_4(){
+    public void RESP_13_TEST_2(){
         Scanner input = new Scanner("3\n");
         StringWriter output = new StringWriter();
 
@@ -104,7 +95,7 @@ class EventCardTest {
 
     @Test
     @DisplayName("Player draws Queen's Favor card and needs to discard 2 cards")
-    public void RESP_6_TEST_5(){
+    public void RESP_13_TEST_3(){
         Scanner input = new Scanner("13\n1\n");
         StringWriter output = new StringWriter();
 
@@ -118,53 +109,6 @@ class EventCardTest {
         game.playEventCard();
 
         Assertions.assertEquals(12, game.getPlayer(game.getTurnCount()).getHand().size());
-    }
-
-    @Test
-    @DisplayName("Player draws Prosperity card and players 1 and 3 need to discard cards")
-    public void RESP_6_TEST_6(){
-        Scanner input = new Scanner("3\n\n\n8\n10\n\n\n");
-        StringWriter output = new StringWriter();
-
-        Game game = new Game(4, input, new PrintWriter(output));
-        game.setUpGame();
-
-        //remove cards from player 1, 2 and 4
-        game.getPlayer(0).discardCard(1);
-        game.getPlayer(1).discardCards(2);
-        game.getPlayer(3).discardCards(12);
-
-        int[] cardsBefore = {game.getPlayer(0).getHand().size(),
-                game.getPlayer(1).getHand().size(),
-                game.getPlayer(2).getHand().size(),
-                game.getPlayer(3).getHand().size()};
-
-        game.getEventDeck().getCards().set(0, new ProsperityCard());
-        game.drawEventCard();
-        game.playEventCard();
-
-        for (int i = 0; i < cardsBefore.length; i++) {
-            Assertions.assertEquals(Math.min(cardsBefore[i] + 2, 12), game.getPlayer(i).getHand().size());
-        }
-    }
-
-    @Test
-    @DisplayName("Player draws Plague card and has less than 2 shields")
-    public void RESP_6_TEST_7(){
-        Scanner input = new Scanner("");
-        StringWriter output = new StringWriter();
-
-        Game game = new Game(4, input, new PrintWriter(output));
-
-        game.setUpGame();
-
-        game.getPlayer(game.getTurnCount()).giveShield(1);
-
-        game.getEventDeck().getCards().set(0, new PlagueCard());
-        game.drawEventCard();
-        game.playEventCard();
-
-        Assertions.assertEquals(0, game.getPlayer(game.getTurnCount()).getShieldNum());
     }
 
 }
